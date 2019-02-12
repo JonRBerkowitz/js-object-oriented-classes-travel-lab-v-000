1cb2223acb93f15a92ffe6097d1c752e39949782
Jonathan Berkowitz <jonrberkowitz@gmail.com>
2019-02-12 20:12:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+let eastWest = [
+  '1st Avenue',
+  '2nd Avenue',
+  '3rd Avenue',
+  'Lexington Avenue',
+  'Park',
+  'Madison Avenue',
+  '5th Avenue'
+];
+
 class Driver {
   constructor(name, startDate) {
     this.name = name;
@@ -5,5 +15,12 @@ class Driver {
   }
   yearsExperienceFromBeginningOf(year) {
     return year - this.startDate.getFullYear();
+  }
+}
+
+class Route {
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation;
+    this.
   }
 }
