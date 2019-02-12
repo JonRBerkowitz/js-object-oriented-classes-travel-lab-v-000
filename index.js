f3f8f3594f7d82ed2cb38e49e95394eb5e029060
Jonathan Berkowitz <jonrberkowitz@gmail.com>
2019-02-12 20:25:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -28,7 +28,10 @@ class Route {
     let verticalDistance = this.endingLocation.vertical - this.beginningLocation.vertical;
     return Math.abs( horizontalDistance ) + Math.abs( verticalDistance );
   }
-  estimatedTime() {
-
-  }
+  estimatedTime(peak) {
+    if ( peak ) {
+        return this.blocksTravelled() / 2;
+      } else {
+        return this.blocksTravelled() / 3;
+      }
 }
